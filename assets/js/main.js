36c1ce1e26f9271990120fcfcd7945dae42072f1
innerermond <innerermond@gmail.com>
2025-06-23 13:13:01 +0530

    updated 4  price section

--- assets/js/main.js
+++ assets/js/main.js
@@ -231,5 +231,10 @@ function sendMessage() {
 }
 
 
-
+ function sendWhatsAppMessage(planName) {
+      const phone = '919747566277';
+      const message = `Hello, I'm interested in the ${planName} plan from Arkisto.`;
+      const url = `https://wa.me/${phone}?text=${encodeURIComponent(message)}`;
+      window.open(url, '_blank');
+    }
 
